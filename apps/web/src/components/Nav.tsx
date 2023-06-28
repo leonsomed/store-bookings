7a0eee436b668a640c5235915d743218854cc279
Leonso Medina <leomedina.me@gmail.com>
2023-06-28 07:11:37 -0600

    nav style

--- apps/web/src/components/Nav.tsx
+++ apps/web/src/components/Nav.tsx
@@ -1,23 +1,19 @@
+import Link from 'next/link';
 import { routes } from '../services/navigation';
-import { Link } from './Link';
 
 export function Nav() {
   return (
-    <nav
-      className="relative flex w-full items-center justify-between bg-blue-600 py-4 text-neutral-600 shadow-lg hover:text-neutral-700 focus:text-neutral-700 dark:bg-zinc-900 dark:text-neutral-200"
-      data-te-navbar-ref
-    >
-      <div
-        className="grow basis-[100%] items-center"
-        id="navbarSupportedContentY"
-        data-te-collapse-item
-      >
-        <ul className="flex items-center justify-center" data-te-navbar-nav-ref>
-          <li data-te-nav-item-ref>
-            <Link href={routes.home()}>Home</Link>
-          </li>
-        </ul>
-      </div>
+    <nav className="relative flex w-full items-center justify-between bg-primary-600 py-4 shadow-lg dark:bg-zinc-900">
+      <ul className="flex justify-start px-6">
+        <li>
+          <Link
+            href={routes.home() as any}
+            className="text-white font-bold uppercase text-xl"
+          >
+            Driving School
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
